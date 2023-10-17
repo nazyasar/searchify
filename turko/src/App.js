755be049bab455341f784888f6c5bc350734967f
Naz Yasar <nazyasar95@gmail.com>
2023-10-17 19:13:52 -0400

    cards added

--- turko/src/App.js
+++ turko/src/App.js
@@ -1,9 +1,47 @@
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, InputGroup, FormControl, Button, Row, Card } from "react-bootstrap";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Card,
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [searchInput, setSearchInput] = useState("");
+
+  return (
+    <div className="App">
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Search for artist"
+            type="input"
+            onKeyPress={(event) => {
+              if (event.key == "Enter") {
+                console.log("Pressed enter");
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <Button onClick={(event) => {}}>Search</Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          <Card>
+            <Card.Img src="#" />
+            <Card.Body>
+              <Card.Title>Album Name</Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
