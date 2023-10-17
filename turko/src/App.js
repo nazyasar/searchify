9071eed1898b112bf284bd925b172d2d677a13db
Naz Yasar <nazyasar95@gmail.com>
2023-10-16 20:24:22 -0400

    project setup

---
+++ turko/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, InputGroup, FormControl, Button, Row, Card } from "react-bootstrap";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
