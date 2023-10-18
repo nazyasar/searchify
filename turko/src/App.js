0c74d1a296a95d7182ce642d3a9dcb10b51c927f
Naz Yasar <nazyasar95@gmail.com>
2023-10-17 21:29:24 -0400

    IT IS WORKING

--- turko/src/App.js
+++ turko/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   Container,
@@ -10,8 +9,73 @@ import {
 } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+
 function App() {
   const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    // API Access Token
+    var authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    };
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then((result) => result.json())
+      .then((data) => setAccessToken(data.access_token));
+  }, []);
+
+  // Search
+  async function search() {
+    console.log("Searching for artist: " + searchInput);
+
+    // GET request using search to get the Artist ID
+    var searchParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    var artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+    console.log("Artist ID is " + artistID);
+
+    // GET request with Artist ID grab all the albums from that artist
+
+    var returnedAlbums = await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums" +
+        "?include_groups=album&market=US&limit=50",
+      searchParameters
+    ) // marketi degistir sonra
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAlbums(data.items);
+      });
+
+    // Display all the albums from the artist
+  }
+  console.log(albums);
 
   return (
     <div className="App">
@@ -22,22 +86,26 @@ function App() {
             type="input"
             onKeyPress={(event) => {
               if (event.key == "Enter") {
-                console.log("Pressed enter");
+                search();
               }
             }}
             onChange={(event) => setSearchInput(event.target.value)}
           />
-          <Button onClick={(event) => {}}>Search</Button>
+          <Button onClick={search}>Search</Button>
         </InputGroup>
       </Container>
       <Container>
         <Row className="mx-2 row row-cols-4">
-          <Card>
-            <Card.Img src="#" />
-            <Card.Body>
-              <Card.Title>Album Name</Card.Title>
-            </Card.Body>
-          </Card>
+          {albums.map((album, i) => {
+            return (
+              <Card key={album.id}>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </Row>
       </Container>
     </div>
